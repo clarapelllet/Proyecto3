b9c5043533f0bd475ca4699b374a91e96fa94c02
clarapelllet <cpelletlastra@udesa.edu.ar>
2023-09-04 17:31:30 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from "react";
 import {Route, Switch} from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer.js";
-import Favoritos from "./components/Favoritos/Favoritos";
-import Generos from "./components/Generos/Generos";
-import Home from "./components/Home/Home";
+
 
 function App() {
   return (
@@ -14,8 +12,8 @@ function App() {
       <main>
     <Switch>
        <Route path="/" component={Home} />
-        <Route path="/favoritos" component={Favoritos} />
-        <Route path="/generos" component={Generos} />
+        {/* <Route path="" component={Favoritos} />
+        <Route path="" component={Generos} /> */}
     </Switch>
     </main>
     <Footer/>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import App from './App';
-=======
 // import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
->>>>>>> 38b6accf5494a800a94eb7ce1d6df327c06e910d
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,8 +14,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-<<<<<<< HEAD
-=======
 // reportWebVitals();
->>>>>>> 38b6accf5494a800a94eb7ce1d6df327c06e910d
 
