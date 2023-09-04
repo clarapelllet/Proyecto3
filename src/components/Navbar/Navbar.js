06a1bbf3d3d75725e858b1b1709c0b78c5413b14
clarapelllet <cpelletlastra@udesa.edu.ar>
2023-09-04 15:35:08 -0300

    header

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,21 @@
 import React from "react";
+import {Link} from "react-router-dom";
 function Navbar(){
     return(
-        <p></p>
+        <header>
+        <nav className="barra"> 
+
+            <ul>
+                <li><img className="logo" src="./img/nuevoLogo.png" alt="logo"/></li>
+                <li><Link to="./index.html">HOME</Link></li>
+                <li><Link to="./favorite.html">FAVORITOS</Link></li>
+                <li><Link to="./genres.html">GÉNEROS</Link></li>
+                <li><form method="get" action="./resultados.html" >
+                    <input type="search" name="buscador" placeholder="Buscar"/>
+                    <input type="submit" value="Buscar"/></form> </li>
+       </ul></nav>
+    </header>
     )
 }
+
+export default Navbar
