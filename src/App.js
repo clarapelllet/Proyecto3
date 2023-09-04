1c70c8b22c9c8be5b46afd7bf9f7663759850441
clarapelllet <cpelletlastra@udesa.edu.ar>
2023-09-04 17:34:40 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
-import {Route, Switch} from 'react-router-dom';
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer.js";
-
-
-=======
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import {Switch,Route} from "react-router-dom";
->>>>>>> 055b1966dcd64a0951276c049825d23845d0c2df
 function App() {
   return (
     <React.Fragment>
@@ -19,13 +11,8 @@ function App() {
       <main>
     <Switch>
        <Route path="/" component={Home} />
-<<<<<<< HEAD
-        {/* <Route path="" component={Favoritos} />
-        <Route path="" component={Generos} /> */}
-=======
         {/* <Route path="/favoritos" component={Favoritos} />
         <Route path="/generos" component={Generos} /> */}
->>>>>>> 055b1966dcd64a0951276c049825d23845d0c2df
     </Switch>
     </main>
     <Home/>
