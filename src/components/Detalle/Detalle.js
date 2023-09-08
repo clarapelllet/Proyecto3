b392db381a839e0fe445350800e1f3453933416e
Antonellaotegui <aotegui@udesa.edu.ar>
2023-09-08 18:04:27 -0300

    detalle avances

--- src/components/Detalle/Detalle.js
+++ src/components/Detalle/Detalle.js
@@ -1,63 +1,32 @@
 
 import React, { Component } from 'react';
 // import {Link} from "react-router-dom";
-import Tarjetas from "../Tarjetas/Tarjetas";
-import Home from "../Home/Home";
+import Tarjetadet from '../Tarjetadet/Tarjetadet';
+// import Home from "../Home/Home";
 
 class Detalle extends Component{
     constructor(){
         super();
         this.state = {
-           peliculaspop : [],
-           peliculastr : []
+          peliculadet:[]
         }
     }
 
     componentDidMount(){
-        fetch(`https://api.themoviedb.org/3/movie/${datosPelicula.id}?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US`)
+        fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US`)
         .then(response => response.json())
         .then(data => this.setState({
-            peliculaspop: data.results
-        }))
-        .catch()
-        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US&page=1`)
-        .then(response => response.json())
-        .then(data => this.setState({
-            peliculastr: data.results
+            peliculadet: data.results
         }))
         .catch()
     }
 
     render(){
-        console.log(this.setState.peliculaspop)
-        console.log(this.setState.peliculastr)
+        console.log(this.setState.peliculadet)
         return(
             <React.Fragment>
-            <article class="infoDetail">
-            <h2 class="tituloDetail"></h2>
-            <p class="descrip"></p>
-            <p class="descrip" id="estreno"></p>
-            <p class="rating"></p>
-            <p class="duracion"></p>
-            <p class="descrip" id="genero"></p>
-            {/* <button class="añadirFav">Añadir a Favoritos</button> */}
-            </article>
-            <section>
-                <h2>Peliculas Populares</h2>
-                { 
-                   this.state.peliculaspop.slice(0,6).map(
-                    (pelicula,i) => <Tarjetas key= {pelicula + i} datosPelicula={pelicula}/>
-                   )
-                }
-            </section>
-            <section>
-                <filtro filtrar={(texto) => this.filtrarPelicula(texto)} />
-                <h2>Peliculas Top Rated</h2>
-                { 
-                   this.state.peliculastr.slice(0,6).map(
-                    (pelicula,i) => <Tarjetas key= {pelicula + i} datosPelicula={pelicula}/>
-                   )
-                }
+            <section className='infoDetail'>
+                <Tarjetadet peliculadet={this.state.peliculadet} genero={this.state.genero} />
             </section>
             </React.Fragment>
             
