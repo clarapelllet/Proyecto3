8305bd0f0689e728508db12b9ec1de213336ef6b
clarapelllet <cpelletlastra@udesa.edu.ar>
2023-09-13 14:54:51 -0300

    not found

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Detalle from "./components/Detalle/Detalle";
-
 // import Filtro from "./components/Filtro/Filtro";
 // import Tarjetas from "./components/Tarjetas/Tarjetas";
 import Valoradas from "./components/Populares/Populares"
 import Populares from "./components/Valoradas/Valoradas"
 import Favoritos from "./components/Favoritos/Favoritos"
-
-
 import {Switch,Route} from "react-router-dom";
 import SearchResults from "./components/SearchResults/SearchResults";
+import NotFound from "./components/Buscador/NotFound/notFound";
+
+
+
 function App() {
   return (
     <React.Fragment>
@@ -24,10 +25,9 @@ function App() {
        <Route path="/Detalle/:id" component={Detalle} />
        <Route path="/Valoradas" component={Valoradas} />
        <Route path="/Populares" component={Populares} />
-
-
        <Route path="/searchResults/:resultado" component={SearchResults}></Route>
       <Route path="/Favoritos" component={Favoritos} />
+      <Route path="" component={NotFound}></Route>
     </Switch>
     </main>
     <Footer/>
